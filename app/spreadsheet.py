0c6d279012bfc017eae81a0149466d33c7b8dd8b
alejandramunoze <atm92@georgetown.edu>
2023-12-10 17:40:30 -0500

    Spreadsheet fetching as a function

--- app/spreadsheet.py
+++ app/spreadsheet.py
@@ -8,27 +8,33 @@ from pprint import pprint
 # Authenticate with google
 gc = gspread.service_account(filename="app\credentials.json")
 
-
-# Open spreadsheet
+# Get the URL
 spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1-NdNLEbaHBToP3eG_dBDNKhYcFeRkDTlwn0-_dK10Zs/edit?usp=sharing'
-workbook = gc.open_by_url(spreadsheet_url)
-sheet = workbook.sheet1  # or workbook.worksheet('SheetName') to access different sheet
 
-# Read data into a Pandas Data Frame
-data = sheet.get_all_values()
-headers = data.pop(0)
-df = pd.DataFrame(data, columns=headers)
+def fetch_spreadsheet(spreadsheet_url):
+    # Open spreadsheet
+    workbook = gc.open_by_url(spreadsheet_url)
+    sheet = workbook.sheet1  # or workbook.worksheet('SheetName') to access different sheet
 
-# Preview Data Frame
-print(df.head())
+    # Read data into a Pandas Data Frame
+    data = sheet.get_all_values()
+    headers = data.pop(0)
+    df = pd.DataFrame(data, columns=headers)
 
-# Convert to DataFrame for easier handling
-df = pd.DataFrame(data[1:], columns=data[0])
+    # Preview Data Frame
+    print(df.head())
 
-# Display data
-print(type(df))
-print(df.columns.tolist())
+    # Convert to DataFrame for easier handling
+    df = pd.DataFrame(data[1:], columns=data[0])
 
-# EG print first row:
-print("ROW #1:")
-pprint(df.iloc[0].to_dict())
+    # Display data
+    print(type(df))
+    print(df.columns.tolist())
+
+    # EG print first row:
+    print("ROW #1:")
+    pprint(df.iloc[0].to_dict())
+
+    return df
+
+fetch_spreadsheet(spreadsheet_url)

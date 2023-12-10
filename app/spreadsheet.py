c13f3112b698bf2c6d07009c8ea1f0a381b86ac9
alejandramunoze <atm92@georgetown.edu>
2023-12-10 15:35:35 -0500

    Update spreadsheet.py

--- app/spreadsheet.py
+++ app/spreadsheet.py
@@ -1,8 +1,8 @@
 # imports at top
 import pandas as pd
 import gspread
-#from google.colab import auth
-# from google.auth import default
+from google.colab import auth
+from google.auth import default
 
 # Authenticate with google
 # auth.authenticate_user()
@@ -20,7 +20,7 @@ headers = data.pop(0)
 df = pd.DataFrame(data, columns=headers)
 
 # Preview Data Frame
-# print(df.head())
+print(df.head())
 
 # Convert to DataFrame for easier handling
 df = pd.DataFrame(data[1:], columns=data[0])

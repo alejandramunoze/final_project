6b9cf7e8df3cda901972dd575c3eb66a345d4862
vsaab1 <vs647@georgetown.edu>
2023-12-10 17:59:18 -0500

    fix

---
+++ website_app/routes/selection_routes.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from app.spreadsheet import fetch_spreadsheet
+
+# Your spreadsheet data (replace this with your actual data)
+spreadsheet_data = fetch_spreadsheet(spreadsheet_url)
+
+@selection_routes.route('/')
+def features():
+    return render_template('selection_form.html')  
+
+@selection_routes.route('/filter', methods=['POST'])
+def filter_data():
+    # Retrieve form data
+    neighbourhood = request.form['neighbourhood']
+    accomodation = request.form['type']
+    single_num = request.form['single_num']
+    
+    # Filter spreadsheet data based on selected data
+    filtered_data = [entry for entry in spreadsheet_data if 
+                     entry['neighbourhood'] == neighbourhood and
+                     entry['type'] == accomodation and
+                     entry['single_num'] == single_num  
+                     ]
+    
+    # Render a template to display filtered data
+    return render_template('filtered_data.html', data=filtered_data, selected_neighbourhood=neighbourhood)

525d6480fac78961165229f270e5059ed70739eb
alejandramunoze <atm92@georgetown.edu>
2023-12-11 16:42:56 -0500

    Connect spreadsheet and routes
    Connected the spreadsheet with the form and result routes. Must now format the results in the "Filtered" page to show the house and the google street view with it.

--- website_app/routes/selection_routes.py
+++ website_app/routes/selection_routes.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, request, Blueprint
+from flask import Flask, render_template, request, Blueprint, flash, redirect
 from app.spreadsheet import fetch_spreadsheet
 
 selection_routes = Blueprint('selection_routes', __name__)
 
 spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1-NdNLEbaHBToP3eG_dBDNKhYcFeRkDTlwn0-_dK10Zs/edit?usp=sharing'
-# Your spreadsheet data (replace this with your actual data)
 
-spreadsheet_data = fetch_spreadsheet(spreadsheet_url)
+
+
 
 @selection_routes.route('/form')
 def features():
@@ -14,17 +14,36 @@ def features():
 
 @selection_routes.route('/filter', methods=['POST'])
 def filter_data():
+    print(request.form)
     # Retrieve form data
     neighbourhood = request.form['neighbourhood']
     accomodation = request.form['type']
-    single_num = request.form['single_num']
-    
+    single_num = request.form['single-num']
+    double_num = request.form['double-num']
+    bathroom_num = request.form['bathroom-num']
+    patio = request.form['patio']
+    parking = request.form['parking']
+
+    # Get spreadsheet data
+    df = fetch_spreadsheet(spreadsheet_url)
+
+    spreadsheet_data = df.to_dict("records")
+
     # Filter spreadsheet data based on selected data
     filtered_data = [entry for entry in spreadsheet_data if 
-                     entry['neighbourhood'] == neighbourhood and
+                     entry['neighborhood'] == neighbourhood and
                      entry['type'] == accomodation and
-                     entry['single_num'] == single_num  
-                     ]
-    
+                     entry['single-num'] == single_num and 
+                     entry['double-num'] == double_num and
+                     entry['bathroom-num'] == bathroom_num and
+                     entry['patio'] == patio and
+                     entry['parking'] == parking
+                      ]
+
+    if any(filtered_data):
     # Render a template to display filtered data
-    return render_template('filtered_data.html', data=filtered_data)
+        return render_template('filtered_data.html', data=filtered_data)
+    else:
+        flash("No matches! Try again","danger")
+        return redirect('/form')
+        
